f9f235adaaa11a0f6af54feeaffd0c2cd21f63c6
James Glenister <jglen419@gmail.com>
2018-12-27 00:00:04 -0500

    Create project3.py
    create Knight's tour program

---
+++ project3.py
@@ -0,0 +1,8 @@
+'''
+The program passes in two numbers: a start position and an end position on a chessboard.
+Program calculates the least number of moves it would take to get from the start to the end if you move as a standard knight
+'''
+
+def answer(src,dest):
+
+    return distance
